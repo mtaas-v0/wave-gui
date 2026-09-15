a1af5f0f2ed7dc4f15f317a4a5abfe8df0848f23
mtaas-v0 <zliu.dayjob@gmail.com>
2026-09-15 16:42:29 -0400

    Update cg_config.cpp

--- src/cg_config.cpp
+++ src/cg_config.cpp
@@ -6,22 +6,14 @@
 #include "cg_config.h"
 
 #include <cstdio>
-#include <cstdarg> // Required for va macros
+#include <cstdarg>
 
-
-namespace CG {
-
-class CG_EXPORT Exception : public std::exception {
-public:
-    /*! \brief The Constructor. */
-    Exception(const char *info, ...) {
-        //_info = new char [1024];
-        va_list ap;
-        va_start(ap, info);
-        vsprintf(_info, info, ap);
-        va_end(ap);
-    }
-    ~Exception() throw() {}
+CG::Exception::Exception(const char *info, ...) {
+    //_info = new char [1024];
+    va_list ap;
+    va_start(ap, info);
+    vsprintf(_info, info, ap);
+    va_end(ap);
 }
 
-} //namespace CG 
+CG::Exception::~Exception() throw() {}
